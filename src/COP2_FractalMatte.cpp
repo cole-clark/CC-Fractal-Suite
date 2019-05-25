d99cbfb10294b4dfd42a62ddb6a7d49b7fc924e1
colevfx <colevfx@gmail.com>
2019-05-25 17:41:17 -0400

    Changed to Pixel Function Skeleton Code

--- src/COP2_FractalMatte.cpp
+++ src/COP2_FractalMatte.cpp
@@ -1,144 +1,145 @@
 /*
 	Cole Clark's Fractal Suite
 
-	COP2_Mandelbrot.cpp
-	Code for CC Fractal Matte Filter Cop Node.
+	COP2_FractalMatte.h
+	Code for CC Fractal Matte Cop filter node.
  */
 
+#include <UT/UT_WorkBuffer.h>
+#include <CH/CH_Manager.h>
+#include <OP/OP_Context.h>
+#include <OP/OP_OperatorTable.h>
+#include <PRM/PRM_Include.h>
+#include <PRM/PRM_Parm.h>
+#include <RU/RU_PixelFunctions.h>
 #include "COP2_FractalMatte.h"
 
-#include <COP2/COP2_CookAreaInfo.h>
-#include <PRM/PRM_Include.h>
-
 using namespace CC;
-
-COP_MASK_SWITCHER(1, "Mattes");
-
-static PRM_Name nameModulo ("modulo", "Modulo");
-
+COP_PIXEL_OP_SWITCHER(1, "Sample Pixel Add");
+static PRM_Name names[] =
+{
+	PRM_Name("addend",  "Add Value"),
+};
+static PRM_Range  addRange(PRM_RANGE_UI, -1, PRM_RANGE_UI, 1);
 PRM_Template
 COP2_FractalMatte::myTemplateList[] =
 {
-	PRM_Template(PRM_SWITCHER, 3, &PRMswitcherName, switcher),
-	PRM_Template(PRM_INT_J, 1, &nameModulo, PRMzeroDefaults),
-	PRM_Template()
+	PRM_Template(PRM_SWITCHER,  3, &PRMswitcherName, switcher),
+	PRM_Template(PRM_RGB_J,     TOOL_PARM, 4, &names[0], PRMoneDefaults,0,
+				 &addRange),
+
+	PRM_Template(),
 };
-
 OP_TemplatePair COP2_FractalMatte::myTemplatePair(COP2_FractalMatte::myTemplateList,
-	&COP2_MaskOp::myTemplatePair);
-
-OP_VariablePair COP2_FractalMatte::myVariablePair(0, &COP2_Node::myVariablePair);
-
-const char* COP2_FractalMatte::myInputLabels[] =
+	&COP2_PixelOp::myTemplatePair);
+OP_VariablePair COP2_FractalMatte::myVariablePair(0, &COP2_MaskOp::myVariablePair);
+const char *    COP2_FractalMatte::myInputLabels[] =
 {
-	"Fractal",
+	"Image to Add to",
 	"Mask Input",
 	0
 };
 
-OP_Node*
-COP2_FractalMatte::myConstructor(
-	OP_Network* net,
-	const char* name,
-	OP_Operator* op)
+OP_Node *
+COP2_FractalMatte::myConstructor(OP_Network *net,
+	const char   *name,
+	OP_Operator  *op)
 {
 	return new COP2_FractalMatte(net, name, op);
 }
-
-COP2_FractalMatte::COP2_FractalMatte(
-	OP_Network* net,
-	const char* name,
-	OP_Operator* op) : COP2_MaskOp(net, name, op) {}
-
-
-COP2_FractalMatte::~COP2_FractalMatte() {}
-
-COP2_ContextData*
-COP2_FractalMatte::newContextData(
-	const TIL_Plane*,
-	int,
-	float t,
-	int xres,
-	int yres,
-	int,
-	int)
+COP2_FractalMatte::COP2_FractalMatte(OP_Network *parent,
+	const char *name,
+	OP_Operator *entry)
+	: COP2_PixelOp(parent, name, entry)
 {
-	COP2_FractalMatteData* data = new COP2_FractalMatteData();
-
-	return data;
 }
+COP2_FractalMatte::~COP2_FractalMatte() { }
+// this is the pixel function, which does the work.
 
-/// Expand the image bounds by one.
-void
-COP2_FractalMatte::computeImageBounds(COP2_Context& context)
+	class cop2_AddFunc : public RU_PixelFunction
+	{
+	public:
+		cop2_AddFunc(float r, float g, float b, float a)
+		{
+			myAddend[0] = r; myAddend[1] = g; myAddend[2] = b; myAddend[3] = a;
+		}
+	protected:
+		// the operation differs per component.
+		virtual bool         eachComponentDifferent() const { return true; }
+		// we don't need to process all the compoents together, as a vector.
+		virtual bool         needAllComponents() const { return false; }
+
+		// Here's the heart of the pixel function - it simply adds our addend to
+		// the passed in val for the given component. This is the scalar version.
+		static float         add(RU_PixelFunction *pf, float val, int comp)
+		{
+			return val + ((cop2_AddFunc*)pf)->myAddend[comp];
+		}
+		// we return the static function above as our scalar function.
+		virtual RUPixelFunc  getPixelFunction() const { return add; }
+		// --or-- 
+		// These are the methods you would use for a vector function. They are
+		// not used in this case, since needAllComponents is false.
+		// You can define a function with both a scalar and a vector function,
+		// and switch between the two based on parameters.
+
+		static void                 addvec(RU_PixelFunction *f, float **vals,
+			const bool *scope)
+		{
+			cop2_AddFunc *pf = (cop2_AddFunc *)f;
+
+			if (vals[0] && scope[0]) *vals[0] = *vals[0] + pf->myAddend[0];
+			if (vals[1] && scope[1]) *vals[1] = *vals[1] + pf->myAddend[1];
+			if (vals[2] && scope[2]) *vals[2] = *vals[2] + pf->myAddend[2];
+			if (vals[3] && scope[3]) *vals[3] = *vals[3] + pf->myAddend[3];
+		}
+
+		// we return the static function above as our vector function.
+		virtual RUVectorFunc        getVectorFunction() const { return addvec; }
+
+	private:
+		float       myAddend[4];
+	};
+
+// This is where we create and return our a new pixel function, defined above.
+// Parms can be evaluated in this method.
+RU_PixelFunction *
+COP2_FractalMatte::addPixelFunction(const TIL_Plane*plane, int, float t, int, int, int)
 {
-	int x1, x2, y1, y2;
-	// Grab the bounds of the mask op, which combines mask with image bounds.
-	COP2_MaskOp::computeImageBounds(context);
-	// Enlarge by 1.
-	context.getImageBounds(x1, y1, x2, y2);
-	context.setImageBounds(x1 - 1, y1 - 1, x2 + 1, y2 + 1);
+	// The frame scope effect is only used if parms on the frame scope page
+	// are altered.
+	int   index = mySequence.getImageIndex(t);
+	float effect = getFrameScopeEffect(index);
+	float r, g, b, a;
+	// Note that we treat the alpha plane differently than other planes.
+	if (plane->isAlphaPlane())
+	{
+		// use the alpha value for the alpha plane.
+		r = g = b = a = ADD(3, t) * effect;
+	}
+	else
+	{
+		// all other planes, use comps 0-3.
+		r = ADD(0, t) * effect;
+		g = ADD(1, t) * effect;
+		b = ADD(2, t) * effect;
+		a = ADD(3, t) * effect;
+	}
+	return new cop2_AddFunc(r, g, b, a);
 }
-
-/// Tell the scheduler what parts of the input's image data is required.
-void
-COP2_FractalMatte::getInputDependenciesForOutputArea(
-	COP2_CookAreaInfo& output_area,
-	const COP2_CookAreaList& input_areas,
-	COP2_CookAreaList& needed_areas)
-{
-	// Add dependencies on the first input and the mask plane.
-	COP2_MaskOp::getInputDependenciesForOutputArea(
-		output_area, input_areas, needed_areas);
-
-	// If bypassed, do nothing.
-	if (getBypass())
-		return;
-
-	// Enlarge the needed area of the first input by 1 pixel in all directions.
-	COP2_CookAreaInfo* inarea =
-		makeOutputAreaDependOnMyPlane(0, output_area, input_areas, needed_areas);
-
-	// May not exist if the input node has an error.
-	if (inarea)
-		inarea->expandNeededArea(1, 1, 1, 1);
-}
-
-/// Friendly node-info.
-const char*
+const char *
 COP2_FractalMatte::getOperationInfo()
 {
-	return "This operation returns mattes from a fractal's integer output.";
-}
+	// return a small string describing what this function does in the info
+	// popup.
+	fpreal t = CHgetEvalTime();
+	int   index = mySequence.getImageIndex(t);
+	float effect = getFrameScopeEffect(index);
 
-OP_ERROR
-COP2_FractalMatte::doCookMyTile(COP2_Context& context, TIL_TileList* tiles)
-{
-	// Grab our context data.
-	COP2_FractalMatteData* data = static_cast<COP2_FractalMatteData *>(context.data());
+	static UT_WorkBuffer info;
+	info.sprintf("Add (%g, %g, %g, %g)",
+		ADD(0, t)*effect, ADD(1, t)*effect,
+		ADD(2, t)*effect, ADD(3, t)*effect);
 
-	TIL_Region* in = inputRegion(0, context,
-		tiles->myX1 - 1,
-		tiles->myY1 - 1,
-		tiles->myX2 + 1,
-		tiles->myY2 + 1,
-		TIL_HOLD);
-
-	if (!in)
-	{
-		tiles->clearToBlack();
-		return error();
-	}
-
-	// Call the operation
-
-	return error();
-}
-
-CC::COP2_FractalMatteData::COP2_FractalMatteData()
-{
-}
-
-CC::COP2_FractalMatteData::~COP2_FractalMatteData()
-{
+	return info.buffer();
 }
--- src/COP2_FractalMatte.h
+++ src/COP2_FractalMatte.h
@@ -5,75 +5,39 @@
 	Header for CC Fractal Matte Cop filter node.
  */
 
-#pragma once
-
-#include <COP2/COP2_MaskOp.h>
-
-namespace CC
-{
-	class COP2_FractalMatte : COP2_MaskOp
+class RU_PixelFunction;
+#include <COP2/COP2_PixelOp.h>
+namespace CC {
+	class COP2_FractalMatte : public COP2_PixelOp
 	{
 	public:
-		/// Node created by myConstructor method.
-		static OP_Node* myConstructor(
-			OP_Network*, const char*, OP_Operator*);
 
-		/// Interface
-		static OP_TemplatePair myTemplatePair;
-		static OP_VariablePair myVariablePair;
-		static PRM_Template myTemplateList[];
-		static CH_LocalVariable myVariableList[];
-		static const char* myInputLabels[];
-
-		/// Given an area to cook, indicate what parts of an image's input
-		/// are needed.
-		virtual void getInputDependenciesForOutputArea
-		(
-			COP2_CookAreaInfo& output_area,
-			const COP2_CookAreaList& input_areas,
-			COP2_CookAreaList& needed_areas
-		);
-
+		static OP_Node              *myConstructor(OP_Network*, const char *,
+			OP_Operator *);
+		static OP_TemplatePair       myTemplatePair;
+		static OP_VariablePair       myVariablePair;
+		static PRM_Template          myTemplateList[];
+		static CH_LocalVariable      myVariableList[];
+		static const char *          myInputLabels[];
 	protected:
-
-		/// Expands the canvas bounds by 1 pixel in all directions.
-		/// computeImageBounds() announces this to the cop engine.
-		virtual void computeImageBounds(COP2_Context& context);
-
-		/// Returns a new context instance with the parameters for the filter
-		/// in it, to be used by the many threads on the tiles.
-		virtual COP2_ContextData* newContextData(
-			const TIL_Plane* p,
-			int array_index,
-			float t,
-			int xres,
-			int yres,
-			int thread,
-			int max_threads);
-
-		/// COP2_MaskOp defines lots of nice mask blending operations in
-		/// cookMyTile(), and defines doCookMyTile for us to override instead.
-		/// This is where the actual image operation is performed.
-		virtual OP_ERROR doCookMyTile(
-			COP2_Context& context, TIL_TileList* tileList);
-
-		/// Returns a description of the operation for the node info popup.
-		virtual const char* getOperationInfo();
-
-		virtual ~COP2_FractalMatte();
+		/// This is the only function we need to override for a pixel function.
+		/// It returns our pixel function, which must be derived from
+		/// RU_PixelFunction.
+		virtual RU_PixelFunction    *addPixelFunction(const TIL_Plane *, int,
+			float t, int, int,
+			int thread);
 
 	private:
-		/// Constructor
-		COP2_FractalMatte(
-			OP_Network* parent, const char* name, OP_Operator* entry);
+		COP2_FractalMatte(OP_Network *parent, const char *name,
+			OP_Operator *entry);
+		virtual     ~COP2_FractalMatte();
+
+		/// An optional method which returns a short description of what this node
+		/// does in the OP info popup.
+		virtual const char  *getOperationInfo();
+		fpreal      ADD(int comp, fpreal t)
+		{
+			return evalFloat("addend", comp, t);
+		}
 	};
-
-	struct COP2_FractalMatteData : public COP2_ContextData
-	{
-		COP2_FractalMatteData();
-
-		virtual ~COP2_FractalMatteData();
-
-
-	};
-};
+} // End HDK_Sample namespace
